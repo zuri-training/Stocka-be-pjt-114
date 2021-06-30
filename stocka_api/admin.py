96ca212a9ff8f6605b19b6699ee96794d4fee816
Brodaoj <olujas1@gmail.com>
2021-06-30 20:43:06 +0100

    fix stocka admin setup

--- stocka_api/admin.py
+++ stocka_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from stocka_api.models import CustomUserManager, User, Inventory, Venture
+
+@admin.register(CustomUserManager)
+class CustomUserManagerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Venture)
+class VentureAdmin(admin.ModelAdmin):
+    pass
