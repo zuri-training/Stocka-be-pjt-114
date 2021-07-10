5cb151df40823e338354864b3b59a848e2f98bf4
iBoBoTi <iroegbucalistus@gmail.com>
2021-07-10 12:00:46 +0100

    conflicting migrations fixed

--- stocka_project/settings.py
+++ stocka_project/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
 EMAIL_PORT = config('EMAIL_PORT')
 
 
